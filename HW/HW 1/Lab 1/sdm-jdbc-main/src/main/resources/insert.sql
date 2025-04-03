40e727d1899208fa2a472cc53ff408b8e7c1979f
mmswflow-upb <152873071+mmswflow-upb@users.noreply.github.com>
2025-04-03 12:19:48 +0300

    Delete insert.sql

--- HW/HW 1/Lab 1/sdm-jdbc-main/src/main/resources/insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO addresses (city, street) VALUES ('Bucuresti', 'Politehnicii');
-INSERT INTO addresses (city, street) VALUES ('Ploiesti', 'Independetei');
-INSERT INTO persons (name, address) VALUES ('Vasile', 1);
-INSERT INTO persons (name, address) VALUES ('John', 1);
-INSERT INTO persons (name, address) VALUES ('Emily', 2);
